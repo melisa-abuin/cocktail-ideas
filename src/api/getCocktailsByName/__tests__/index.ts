78ecb2fea56e0b2e54778a454d53f8f0d510b059
melisa-abuin <melisa.abuin@gmail.com>
2023-02-02 22:10:53 +0100

    feature #2: add test for api fail case

--- src/api/getCocktailsByName/__tests__/index.ts
+++ src/api/getCocktailsByName/__tests__/index.ts
@@ -19,6 +19,16 @@ describe('getCocktailsByName', () => {
     expect(result).toMatchObject({ drinks: [] })
   })
 
-  it.todo('returns null when there is an error')
+  it('returns null when there is an error', async () => {
+
+    global.fetch = jest.fn(() =>
+      Promise.reject()
+    ) as jest.Mock
+
+    const result = await getCocktailsByName('vodka')
+
+    expect(global.fetch).toHaveBeenCalled()
+    expect(result).toBe(null)
+  })
 
 })
