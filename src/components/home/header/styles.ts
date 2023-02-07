d3e32ecdde5044cb0528da552fa477b081b9d67c
melisa-abuin <melisa.abuin@gmail.com>
2023-02-07 18:07:03 +0100

    feature #15: add header to home page

---
+++ src/components/home/header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Description = styled.p`
+  color: #000000;
+  font-family: 'Inter-Regular';
+  font-size: 14px;
+  text-align: center;
+`
+
+export const Title = styled.h1`
+  color: #FF0000;
+  font-family: 'Inter-Regular';
+  font-size: 48px;
+  line-height: 48px;
+  margin: 40px 0 20px 0;
+`
--- src/components/home/searchbox/styles.ts
+++ src/components/home/searchbox/styles.ts
@@ -1,20 +1,23 @@
 import styled from 'styled-components'
 
 export const Addon = styled.div`
-  margin-left: -32px;
+  position: absolute;
+  right: 24px;
+  top: 16px;
 `
 
 export const Container = styled.div`
   align-items: center;
   display: flex;
   flex-direction: row;
+  max-width: 470px;
   position: relative;
+  width: 100%;
 `
 
 export const Input = styled.input`
   border: 1px solid #787878;
   border-radius: 30px;
-  max-width: 304px;
-  padding: 10px 18px;
-  width: 100%;
+  flex: 1;
+  padding: 16px 24px;
 `
