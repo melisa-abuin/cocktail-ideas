52d0f50bba3bf5c92cac8c476d404e132f37ea07
melisa-abuin <melisa.abuin@gmail.com>
2023-02-10 13:03:02 +0100

    fix #21: fix api url formatter switch cases

--- src/utils/apiUrlFormatter/index.ts
+++ src/utils/apiUrlFormatter/index.ts
@@ -9,7 +9,7 @@ export const apiUrlFormatter = ({ entity, param }: Props) => {
   switch(entity){
     case 'cocktail':
       return `${apiBaseUrl}/search.php?s=${param}`
-    case 'cocktail':
+    case 'ingredient':
       return `${apiBaseUrl}/search.php?i=${param}`
     default:
       return apiBaseUrl
