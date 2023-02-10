a5303d7e9f96dc88958170438bf8de32a83ea107
melisa-abuin <melisa.abuin@gmail.com>
2023-02-10 14:51:45 +0100

    feature #21: improve get cocktails by ingredient api

--- src/api/getCocktailsByIngredient/__tests__/index.ts
+++ src/api/getCocktailsByIngredient/__tests__/index.ts
@@ -6,17 +6,18 @@ afterEach(() => {
 
 describe('getCocktailsByIngredient', () => {
   it('returns the drinks object when api call is successful', async () => {
+    const mockedCocktails = [{ idDrink: "1", strDrink: "drink name", strDrinkThumb: "https://drink-image.jpg"}] 
 
     global.fetch = jest.fn(() =>
       Promise.resolve({
-        json: () => Promise.resolve({ drinks: [] }),
+        json: () => Promise.resolve({ drinks: mockedCocktails }),
       }),
     ) as jest.Mock
 
     const result = await getCocktailsByIngredient('vodka')
 
     expect(global.fetch).toHaveBeenCalled()
-    expect(result).toMatchObject([])
+    expect(result).toMatchObject(mockedCocktails)
   })
 
   it('returns null when there is an error', async () => {
--- src/api/getCocktailsByIngredient/index.ts
+++ src/api/getCocktailsByIngredient/index.ts
@@ -1,10 +1,20 @@
-import { pipe } from "fp-ts/function"
+import { flow, pipe } from "fp-ts/function"
 import * as TE from "fp-ts/TaskEither"
 import * as E from "fp-ts/Either"
 import { handleApiError } from "@/src/utils/handleApiError"
 import { apiBaseUrl } from "@/src/constants/api"
+import { drinks } from "@/src/types/drinks"
+import * as t from "io-ts"
+import { failure } from 'io-ts/lib/PathReporter'
 
 const apiUrl = "/filter.php"
+
+const decodeWith = <A>(decoder: t.Decoder<unknown, A>) =>
+  flow(
+    decoder.decode,
+    E.mapLeft(errors => new Error(failure(errors).join('\n'))),
+    TE.fromEither
+  )
 
 export const getCocktailsByIngredient = async (name: string) => {
   const result = await pipe(
@@ -16,10 +26,11 @@ export const getCocktailsByIngredient = async (name: string) => {
       () => response.json(),
       handleApiError,
     )),
+    TE.chain(response => decodeWith(drinks)(response.drinks))
   )()
 
   if(E.isRight(result)){
-    return result.right.drinks
+    return result.right
   }
 
   return null
