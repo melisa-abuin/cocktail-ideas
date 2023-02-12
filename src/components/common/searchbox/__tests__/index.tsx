c1e3d1a4e5da68ff9b53666d7d192b040b52ddbc
melisa-abuin <melisa.abuin@gmail.com>
2023-02-12 15:29:00 +0100

    feature #20: pass onchange function to searchbox

--- src/components/common/searchbox/__tests__/index.tsx
+++ src/components/common/searchbox/__tests__/index.tsx
@@ -1,10 +1,31 @@
 import { SearchBox } from '..'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('SearchBox', () => {
   it('renders the searchbox input', () => {
-    render(<SearchBox />)
+    render(<SearchBox onChange={jest.fn()} />)
 
     expect(screen.getByPlaceholderText('Type ingredient')).toBeInTheDocument()
+  })
+
+  it('renders the default value when passed by props', () => {
+    const value = 'some value'
+    render(<SearchBox defaultValue={value} onChange={jest.fn()} />)
+
+    expect(screen.getByPlaceholderText('Type ingredient')).toHaveValue(value)
+  })
+
+  it('returns the string value of the field on change', async () => {
+    const onChange = jest.fn()
+    render(<SearchBox onChange={onChange} />)
+
+    const input = screen.getByPlaceholderText('Type ingredient')
+
+    userEvent.type(input, 'some value')
+
+    waitFor(() => {
+      expect(onChange).toHaveBeenCalledWith('some value')
+    })
   })
 })
--- src/components/common/searchbox/index.tsx
+++ src/components/common/searchbox/index.tsx
@@ -1,17 +1,28 @@
 import { Addon, Container, Input } from './styles'
 import { SearchIcon } from '@/src/assets/svg/searchicon'
+import { ChangeEvent } from 'react'
 
 type Props = {
   defaultValue?: string
+  onChange: (value: string) => void
   size?: 'medium' | 'small'
 }
 
-export const SearchBox = ({ defaultValue, size = 'medium' }: Props) => {
+export const SearchBox = ({
+  defaultValue,
+  onChange,
+  size = 'medium',
+}: Props) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value)
+  }
+
   return (
     <Container size={size}>
       <Input
         defaultValue={defaultValue}
         inputSize={size}
+        onChange={handleChange}
         placeholder="Type ingredient"
       />
       <Addon size={size}>
