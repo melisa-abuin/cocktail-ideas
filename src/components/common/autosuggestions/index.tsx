14e75a958922090c91032394d21dfede579d9195
melisa-abuin <melisa.abuin@gmail.com>
2023-02-14 22:17:37 +0100

    feature #20: add autosuggest component

---
+++ src/components/common/autosuggestions/index.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import { SearchBox } from '../searchbox'
+import { useQuery } from 'react-query'
+import { Container, Option, Options } from './styles'
+import { getIngredientsByName } from '@/src/api/getIngredientsByName'
+
+type Props = {
+  size?: 'medium' | 'small'
+}
+
+export const AutoSuggestions = ({ size }: Props) => {
+  const [value, setValue] = useState('')
+  const [showOptions, setShowOptions] = useState(false)
+  const [isSelecting, setIsSelecting] = useState(false)
+
+  const { data, refetch } = useQuery(
+    'ingredients',
+    () => getIngredientsByName(value),
+    {
+      enabled: !!value && value.length > 1,
+    }
+  )
+
+  const handleChange = (value: string) => {
+    setShowOptions(true)
+    setValue(value)
+    refetch()
+  }
+
+  const handleOnBlur = () => (isSelecting ? null : setShowOptions(false))
+
+  const shouldCloseOptions = () => setIsSelecting(false)
+  const shouldKeepOptionsOpen = () => setIsSelecting(true)
+
+  const handleSelectOption = (option: string) => {
+    setValue(option)
+    setShowOptions(false)
+  }
+
+  return (
+    <Container onBlur={handleOnBlur}>
+      <SearchBox onChange={handleChange} value={value} size={size} />
+      {data && showOptions && (
+        <Options
+          onMouseEnter={shouldKeepOptionsOpen}
+          onMouseLeave={shouldCloseOptions}
+        >
+          {data.map((suggestions) => (
+            <Option
+              key={suggestions.idIngredient}
+              onClick={() => handleSelectOption(suggestions.strIngredient)}
+            >
+              {suggestions.strIngredient}
+            </Option>
+          ))}
+        </Options>
+      )}
+    </Container>
+  )
+}
