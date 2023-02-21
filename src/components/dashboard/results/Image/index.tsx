19be45521986d572cc9b5d311b3e8b5cecd074eb
melisa-abuin <melisa.abuin@gmail.com>
2023-02-21 14:40:11 +0100

    enhancement #28: move grid errors to a new component

--- src/components/dashboard/results/Image/index.tsx
+++ src/components/dashboard/results/Image/index.tsx
@@ -1,9 +1,9 @@
 import { Cocktail } from '@/src/types/cocktails'
 import { Container, StyledImage, Text } from './styles'
 
-export const Image = ({ idDrink, strDrink, strDrinkThumb }: Cocktail) => (
-  <Container key={idDrink}>
-    <StyledImage alt="banner" src={strDrinkThumb} />
+export const Image = ({ strDrink, strDrinkThumb }: Cocktail) => (
+  <Container>
+    <StyledImage alt="loader" src={strDrinkThumb} />
     <Text>{strDrink}</Text>
   </Container>
 )
---
+++ src/components/dashboard/results/errorMessage/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from '@/src/components/section/text'
+import { Row } from '@/src/components/section/row'
+
+export const ErrorMessage = () => (
+  <Row>
+    <Text type="error">Ups, something went wrong! Try again please.</Text>
+  </Row>
+)
