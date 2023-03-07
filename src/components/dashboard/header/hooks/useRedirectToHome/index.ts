7088586ee7d7227a3d8993d00eaaab5d2342ba42
melisa-abuin <melisa.abuin@gmail.com>
2023-03-07 21:41:39 +0100

    feature #29: add redirect to homepage

---
+++ src/components/dashboard/header/hooks/useRedirectToHome/index.ts
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router'
+import { useCallback } from 'react'
+
+export const useRedirectToHome = () => {
+  const { push } = useRouter()
+
+  return useCallback(
+    () =>
+      push({
+        pathname: '/home',
+      }),
+    [push]
+  )
+}
