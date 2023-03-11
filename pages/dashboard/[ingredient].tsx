e08f24ef24db2b440edf2723534e776aa14ef24e
melisa-abuin <melisa.abuin@gmail.com>
2023-03-11 15:59:45 +0100

    feature #43: add favicon and titles to pages

--- pages/dashboard/[ingredient].tsx
+++ pages/dashboard/[ingredient].tsx
@@ -2,15 +2,22 @@ import { Column } from '@/src/components/section/column'
 import { Header } from '@/src/components/dashboard/header'
 import { Results } from '@/src/components/dashboard/results'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 export default function Dashboard() {
   const router = useRouter()
   const { ingredient } = router.query
 
   return (
-    <Column>
-      <Header />
-      <Results ingredient={ingredient as string} />
-    </Column>
+    <>
+      <Head>
+        <title>Cocktails - Results</title>
+      </Head>
+
+      <Column>
+        <Header />
+        <Results ingredient={ingredient as string} />
+      </Column>
+    </>
   )
 }
