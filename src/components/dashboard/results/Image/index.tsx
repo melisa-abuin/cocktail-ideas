e060b621d0e40c12dd3a5e27fabefa0cb51f04ce
Melisa Abuin <melisa.abuin@gmail.com>
2023-03-11 01:14:43 +0100

    fix #40: delete old image file

--- src/components/dashboard/results/Image/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Cocktail } from '@/src/types/cocktails'
-import { Container, StyledImage, Text } from './styles'
-
-export const Image = ({ strDrink, strDrinkThumb }: Cocktail) => (
-  <Container>
-    <StyledImage alt="loader" src={strDrinkThumb} />
-    <Text>{strDrink}</Text>
-  </Container>
-)
